bd952f52c3c2b61adf1529089a986bc8d27d0424
gabriel <gabriel.pereira.pinheiro@gmail.com>
2020-07-17 20:12:18 -0300

    feat routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
+import { Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Routes
+import HomePage from './containers/HomePage/HomePage'
+
+class App extends Component {
+
+  componentDidMount() {
+  }
+
+  render() {
+
+    let routes = (
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Redirect to="/" />
+      </Switch>
+    )
+
+    return (
+      <div>
+        {routes}
+      </div>
+    );
+  }
 }
 
 export default App;
